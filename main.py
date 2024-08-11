0a765c5ca52e188315caf040b1aae07e089f3599
IT21314742 <it21314742@my.sliit.lk>
2024-08-11 23:04:03 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_user(user_id):
 
 @app.route("create-user", methods=["POST", "GET"])
 def create_user():
-    if request(method, url, body=None, headers=
+    if request(method, url, body=None, headers
 
 
 # GET
