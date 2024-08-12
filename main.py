cafb697b9821b9c1f512df18103dde3673b32b53
IT21314742 <it21314742@my.sliit.lk>
2024-08-12 22:39:11 +0530

    Update
    Update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_user(user_id):
     user_data = {
         "user_id": user_id,
         "name": "Vihan Maneth",
-        "email": "V@gmail.com"
+        "email": "Vi@gmail.com"
     }
     
     extra = request.args.get("extra")
